456986fa2d9e306ca0800b69076d85071e0ae38d
Elias Cole <eliascole3@gmail.com>
2018-01-21 20:01:50 -0800

    added IBSDiets.org fodmap diet

---
+++ script.js
@@ -0,0 +1,208 @@
+// IBS (FODMAP’s)-FM 
+// Interstitial Cystitis-IC 
+// MCAS (low-histamine) -LH 
+
+let fodmap = require('./diets/fodmap.js').data
+// let fodmap = require('./diets/fodmap.js')
+
+// (.+)
+// '$1',
+
+
+console.log(fodmap)
+
+// fodmap.veggies.good.forEach(x => {
+//   console.log(x)
+// })
+
+
+
+
+
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let fodmap = {
+//   veggies: {
+//     good: [
+
+//     ],
+//     maybe: [
+
+//     ],
+//     bad: [
+
+//     ],
+//   },
+//   fruit: {
+//     good: [
+
+//     ],
+//     maybe: [
+
+//     ],
+//     bad: [
+
+//     ],
+//   },
+//   protein: {
+//     good: [
+
+//     ],
+//     maybe: [
+
+//     ],
+//     bad: [
+
+//     ],
+//   },
+//   grains: {
+//     good: [
+
+//     ],
+//     maybe: [
+
+//     ],
+//     bad: [
+
+//     ],
+//   },
+//   dairy: {
+//     good: [
+
+//     ],
+//     maybe: [
+
+//     ],
+//     bad: [
+
+//     ],
+//   },
+//   nuts: {
+//     good: [
+
+//     ],
+//     maybe: [
+
+//     ],
+//     bad: [
+
+//     ],
+//   },
+//   sweeteners: {
+//     good: [
+
+//     ],
+//     maybe: [
+
+//     ],
+//     bad: [
+
+//     ],
+//   },
+//   fats: {
+//     good: [
+
+//     ],
+//     maybe: [
+
+//     ],
+//     bad: [
+
+//     ],
+//   },
+//   seasonings: {
+//     good: [
+
+//     ],
+//     maybe: [
+
+//     ],
+//     bad: [
+
+//     ],
+//   },
+//   beverages: {
+//     good: [
+      
+//     ],
+//     maybe: [
+
+//     ],
+//     bad: [
+
+//     ],
+//   },
+//   miscellaneous: {
+//     good: [
+
+//     ],
+//     maybe: [
+
+//     ],
+//     bad: [
+
+//     ],
+//   },
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
