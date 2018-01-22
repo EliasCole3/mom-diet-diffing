1a817c015044a8656e8e86ace8e26474e61d1709
Elias Cole <eliascole3@gmail.com>
2018-01-21 20:19:22 -0800

    started on the standford FODMAP

---
+++ diets/template.js
@@ -0,0 +1,128 @@
+let data = {
+  veggies: {
+    good: [
+
+    ],
+    maybe: [
+
+    ],
+    bad: [
+
+    ],
+  },
+  fruit: {
+    good: [
+
+    ],
+    maybe: [
+
+    ],
+    bad: [
+
+    ],
+  },
+  protein: {
+    good: [
+
+    ],
+    maybe: [
+
+    ],
+    bad: [
+
+    ],
+  },
+  grains: {
+    good: [
+
+    ],
+    maybe: [
+
+    ],
+    bad: [
+
+    ],
+  },
+  dairy: {
+    good: [
+
+    ],
+    maybe: [
+
+    ],
+    bad: [
+
+    ],
+  },
+  nuts: {
+    good: [
+
+    ],
+    maybe: [
+
+    ],
+    bad: [
+
+    ],
+  },
+  sweeteners: {
+    good: [
+
+    ],
+    maybe: [
+
+    ],
+    bad: [
+
+    ],
+  },
+  fats: {
+    good: [
+
+    ],
+    maybe: [
+
+    ],
+    bad: [
+
+    ],
+  },
+  seasonings: {
+    good: [
+
+    ],
+    maybe: [
+
+    ],
+    bad: [
+
+    ],
+  },
+  beverages: {
+    good: [
+      
+    ],
+    maybe: [
+
+    ],
+    bad: [
+
+    ],
+  },
+  miscellaneous: {
+    good: [
+
+    ],
+    maybe: [
+
+    ],
+    bad: [
+
+    ],
+  },
+}
+
+module.exports = {
+  data: data
+} 
+
